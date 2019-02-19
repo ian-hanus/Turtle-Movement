9725e41e6cd3018d033f0ce1c259e45a30d32cc8
Ian Hanus <ih52@duke.edu>
2019-02-19 00:25:18 -0500

    Added method comments to Config and VariableDisplay

--- src/frontend/Config.java
+++ src/frontend/Config.java
@@ -6,29 +6,41 @@ import javafx.scene.paint.Color;
  * Stores the features of our simulation that should be able to be changed from a series of dropdowns and menus
  */
 public interface Config {
+    /**
+     * Get the current color of the turtle
+     * @return a paint color corresponding to the current color of the turtle
+     */
+    Color getTurtleColor();
 
-    default public Color getTurtleColor(){
-        return this.getTurtleColor();
+    /**
+     * Get the current color of the pen
+     * @return a paint color corresponding to the current color of the pen
+     */
+    Color getPenColor();
+
+    /**
+     * Reset the configuration to all of the defaults
+     */
+    void resetConfig();
+
+    /**
+     * Set the color of the turtle
+     * @param c is the paint color of the turtle
+     */
+    void setTurtleColor(Color c);
+
+    /**
+     * Set the color of the pen
+     * @param c is the paint color of the pen
+     */
+    void setPenColor(Color c);
+
+    /**
+     * Set the language that will be coded into to translate the commands
+     * @param s is the string representing the language
+     */
+    default public void setLanguage(String s){
+        this.setLanguage(s);
     }
-
-    default public Color getPenColor(){
-        return this.getPenColor();
-    }
-
-    default public void resetConfig(){
-        this.resetConfig();
-    }
-
-//    default public void setTurtleColor(Color c){
-//        this.setTurtleColor(c);
-//    }
-//
-//    default public void setPenColor(Color c){
-//        this.setPenColor(c);
-//    }
-//
-//    default public void setLanguage(String s){
-//        this.setLanguage(s);
-//    }
 
 }
