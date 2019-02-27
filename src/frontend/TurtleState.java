a1b181dae9518c2b692bb3c8a661732d4f8def86
Sachal Dhillon <sachal.dhillon@gmail.com>
2019-02-27 10:56:35 -0500

    updated turtlestate to accomodate copies

--- src/frontend/TurtleState.java
+++ src/frontend/TurtleState.java
@@ -1,15 +1,13 @@
 package frontend;
 
-import backend.Turtle;
-
 public class TurtleState {
+
     private int x;
     private int y;
     private double angle;
     private boolean visible;
     private boolean down;
-
-
+    private boolean shouldReset;
 
     public TurtleState(int xpos, int ypos, double orientation, boolean isVisible, boolean isDown){
         x = xpos;
@@ -17,14 +15,21 @@ public class TurtleState {
         angle = orientation;
         visible = isVisible;
         down = isDown;
+        shouldReset = false;
     }
 
-
+    public TurtleState(TurtleState toCopy){
+        this.x = toCopy.getX();
+        this.y = toCopy.getY();
+        this.angle = toCopy.getAngle();
+        this.visible = toCopy.isVisible();
+        this.down = toCopy.isDown();
+        this.shouldReset = toCopy.shouldReset();
+    }
 
     public int getX() {
         return x;
     }
-
     public int getY() {
         return y;
     }
@@ -34,9 +39,20 @@ public class TurtleState {
     public boolean isVisible() {
         return visible;
     }
-
     public boolean isDown() {
         return down;
     }
+    public boolean shouldReset() { return shouldReset; }
+
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    public void setAngle(double angle) { this.angle = angle; }
+    public void setVisibility(boolean visible) { this.visible = visible; }
+    public void setDown(boolean down) { this.down = down; }
+    public void setShouldReset(boolean shouldReset) { this.shouldReset = shouldReset; }
+
+
+
+
 
 }
