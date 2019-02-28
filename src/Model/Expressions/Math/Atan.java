323d3bd26b566b28df780f8ff1a198061d9d21f4
Sachal Dhillon <sachal.dhillon@gmail.com>
2019-02-27 23:12:54 -0500

    Added Tan, Atan

---
+++ src/Model/Expressions/Math/Atan.java
@@ -0,0 +1,33 @@
+package Model.Expressions.Math;
+import Model.Exceptions.UninitializedExpressionException;
+import Model.Expressions.Expression;
+import Model.Exceptions.AlteringExpressionException;
+import java.lang.Math;
+
+public class Atan extends Expression {
+
+    private Expression degrees;
+
+    public Atan(Expression degrees) throws AlteringExpressionException
+    {
+        setArguments(degrees);
+    }
+
+    public void setArguments(Expression degrees) throws AlteringExpressionException{
+        finalizeStates();
+        this.degrees = degrees;
+    }
+
+    @Override
+    public double evaluate() throws UninitializedExpressionException {
+        checkInitialization();
+        double radians = degrees.evaluate()/180*Math.PI;
+        return Math.atan(radians);
+    }
+
+    @Override
+    public Class[] getArgumentTypes() {
+        Class expression = super.getClass();
+        return new Class[]{expression};
+    }
+}
