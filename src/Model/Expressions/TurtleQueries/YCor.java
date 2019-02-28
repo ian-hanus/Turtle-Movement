f3646065fa6d1a5ad7fbb04401938e5380f5a02f
Sachal Dhillon <sachal.dhillon@gmail.com>
2019-02-28 00:29:44 -0500

    Added Turtle Queries

---
+++ src/Model/Expressions/TurtleQueries/YCor.java
@@ -0,0 +1,33 @@
+package Model.Expressions.TurtleQueries;
+import Model.Exceptions.UninitializedExpressionException;
+import Model.Expressions.Expression;
+import Model.Exceptions.AlteringExpressionException;
+import java.util.Deque;
+import frontend.TurtleState;
+
+public class YCor extends Expression {
+
+    private Deque<TurtleState> queue;
+
+    public YCor(Deque<TurtleState> queue) throws AlteringExpressionException
+    {
+        setArguments(queue);
+    }
+
+    public void setArguments(Deque<TurtleState> queue) throws AlteringExpressionException{
+        finalizeStates();
+        this.queue = queue;
+    }
+
+    @Override
+    public double evaluate() throws UninitializedExpressionException {
+        checkInitialization();
+        TurtleState copy = new TurtleState(queue.getLast());
+        return copy.getY();
+    }
+
+    @Override
+    public Class[] getArgumentTypes() {
+        return new Class[]{java.util.Deque.class};
+    }
+}
