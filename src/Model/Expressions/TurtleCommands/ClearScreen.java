4dee32117f587eae984dc084614d20872f05ae5e
Sachal Dhillon <sachal.dhillon@gmail.com>
2019-03-07 22:40:31 -0500

    Added branch statements and procedures

--- src/Model/Expressions/TurtleCommands/ClearScreen.java
+++ src/Model/Expressions/TurtleCommands/ClearScreen.java
@@ -21,9 +21,17 @@ public class ClearScreen implements Expression, ExpressionTaker, TurtleExpressio
     @Override
     public double evaluate() {
         TurtleState copy = new TurtleState(queue.getLast());
+        double oldX = copy.getX();
+        double oldY = copy.getY();
+        double newX = 0;
+        double newY = 0;
+        copy.setX(newX);
+        copy.setY(newY);
         copy.setShouldReset(true);
+        copy.setAngle(90);
         queue.addLast(copy);
-        return 1;
+        double distance = Math.sqrt(Math.pow(oldX-newX,2)+ Math.pow(oldY-newY,2));
+        return distance;
     }
 
     public int getDefaultNumExpressions(){
