49ec284c237e8c4c7aa32a74fb8f16a314fe118d
Sachal Dhillon <sachal.dhillon@gmail.com>
2019-03-07 21:04:46 -0500

    Added branch statements

--- src/Model/Expressions/Controls/If.java
+++ src/Model/Expressions/Controls/If.java
@@ -1,4 +1,35 @@
 package Model.Expressions.Controls;
+import Model.Expressions.Basic.Constant;
+import Model.Expressions.Interfaces.Expression;
+import Model.Expressions.Interfaces.VariableArgumentTaker;
 
-public class If {
+import java.util.Map;
+
+public class If implements Expression, VariableArgumentTaker {
+
+    private Expression[] inputs;
+
+    public If(String variable, Map<String, Constant> variableMap, Expression... inputs){
+        if(inputs.length < getDefaultNumExpressions()){
+            throw new IllegalArgumentException(String.format("At least %d Expressions required", getDefaultNumExpressions()));
+        }
+        this.inputs = inputs;
+    }
+
+    @Override
+    public double evaluate(){
+        double out = 0;
+        if(inputs[0].evaluate()!=0){
+            for(int x=1;x<inputs.length;x++){
+                out=inputs[x].evaluate();
+            }
+        }
+        return out;
+    }
+
+    @Override
+    public int getDefaultNumExpressions() {
+        return 1;
+    }
+
 }
